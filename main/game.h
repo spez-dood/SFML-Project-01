a9e23a37dba3af8c57ee7f8b5fed5736d6577da2
Spez <jluke5664@gmail.com>
2025-12-01 11:27:18 -0800

    Small Refactor to account for changes elsewhere
    Broke up some loops for updating/drawing gui into indicvidual methods.

--- main/game.h
+++ main/game.h
@@ -10,22 +10,20 @@ class Game {
 public:
     Game();
 
-    int loop();
+    int Run();
 protected:
+    void GetSettings(SI_Error rc);
     void build_settingsINI(SI_Error rc);
     void find_clicked(sf::Event event);
     void find_mouse_move(sf::Event event);
     void get_event(sf::Event event);
     void key_pressed(sf::Keyboard::Scancode keycode);
     void handle_events();
+    void UpdateGui();
+    void DrawGui();
+    void DrawText(auto given);
+    void loop();
 
-    void NewButton(
-    const std::string& name = "TextButton",
-    obj::ShapeSize size = sf::Vector2f{200.f,90.f},
-    sf::Vector2f pos = sf::Vector2f{400.f, 300.f},
-    sf::Color fillColor = sf::Color::White,
-    obj::GuiAttachment = "Click me!"
-    );
 private:
     GameState state = GameState::MainMenu;
     obj::GuiVector ui_objects;
