b28087d0453b3edab23a1d04c5a1c5c00bb4d99f
Spez <jluke5664@gmail.com>
2025-12-05 17:17:07 -0800

    Added GameState, Gui Instantiation prep methods
    Added new GameState enum for tracking progress.
    
    Added recursive method which instantiates Gui.
    - can take in predefined, or fresh GuiConfigs
    - can take in GuiConfigs, either fresh or predefined, and offset their window position to:
        * Center
        *Top-Center

--- main/game.h
+++ main/game.h
@@ -2,9 +2,12 @@
 #define game_h
 
 #include "config.h" // SimpleIni tools
-#include "obj.h"
+#include "gui.h"
+// #include "entity.h"
 
-enum class GameState { MainMenu, Playing, GameOver };
+using namespace gui;
+
+enum class GameState { Boot, StartScreen, MainMenu, Playing, GameOver };
 
 class Game {
 public:
@@ -13,20 +16,69 @@ public:
     int Run();
 protected:
     void GetSettings(SI_Error rc);
-    void build_settingsINI(SI_Error rc);
-    void find_clicked(sf::Event event);
-    void find_mouse_move(sf::Event event);
+    void build_settingsINI(SI_Error rc, CSimpleIniA& def_config, SI_Error def_rc);
+    void find_clicked(sf::Event::MouseButtonEvent mouseButton);
+    void find_mouse_move(sf::Event::MouseMoveEvent mouseMove);
     void get_event(sf::Event event);
     void key_pressed(sf::Keyboard::Scancode keycode);
+
+    void Setup_StartScreen();
+    void Setup_MainMenu();
+    void Setup_Gameplay();
+
+    void handle_state_change();
     void handle_events();
     void UpdateGui();
     void DrawGui();
-    void DrawText(auto& given);
     void loop();
 
+    gui::GuiConfig Prep(
+        gui::GuiConfig cfg, 
+        PrepGui posCalc = {},
+        sf::Vector2f offset = {0.f, 0.f}
+    ) const {
+        if (posCalc) {
+            cfg.pos = posCalc(cfg);
+            return cfg;
+        }
+
+        auto s = cfg.SizeVec();
+        cfg.pos = {
+            (static_cast<float>(this->window.getSize().x) - s.x) / offset.x,
+            (static_cast<float>(this->window.getSize().y) - s.y) / offset.y
+        };
+        return cfg;
+    };
+
+    gui::GuiConfig Prep(
+        gui::GuiConfig cfg,
+        sf::Vector2f (Game::*posCalc)(const gui::GuiConfig&) const
+    ) const {
+        if (posCalc) {
+            cfg.pos = (this->*posCalc)(cfg);
+            return cfg;
+        }
+        return Prep(std::move(cfg), PrepGui());
+    };
+
+    sf::Vector2f CenterGui(const gui::GuiConfig& cfg) const {
+        return sf::Vector2f{
+            (this->window.getSize().x - cfg.SizeVec().x) / 2.f,
+            (this->window.getSize().y - cfg.SizeVec().y) / 2.f
+        };
+    }
+
+    sf::Vector2f TopCenterGui(const gui::GuiConfig& cfg) const {
+        return sf::Vector2f{
+            (this->window.getSize().x - cfg.SizeVec().x) / 2.f,
+            (this->window.getSize().y - cfg.SizeVec().y) / 5.f
+        };
+    }
+
 private:
-    GameState state = GameState::MainMenu;
-    obj::GuiVector ui_objects;
+    GameState state = GameState::Boot;
+    bool state_changed = false;
+    gui::GuiVector ui_objects;
     sf::RenderWindow window;
     CSimpleIniA config;
 };
